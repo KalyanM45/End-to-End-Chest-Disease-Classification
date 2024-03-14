61f565b685e54f5779b22d40e5e0ac7e424100bf
KalyanMurapaka45 <kalyanmurapaka274@gmail.com>
2024-03-14 22:25:29 +0530

    Project Requirement Setup Update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ChestDiseaseClassifier",
+    version="0.0.1",
+    author="KalyanM45",
+    author_email="kalyanmurapaka274@gmail.com",
+    url=f"https://github.com/KalyanM45/End-to-End-Chest-Disease-Classification",
+    packages=find_packages(),   
+)
